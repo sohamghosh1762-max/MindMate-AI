99a8fcd7ad24517a9127c95122c00db331af153d
sohamghosh1762-max <sohamghosh1762@gmail.com>
2026-05-21 22:39:29 +0530

    Fixed Render Flask port

--- server/app.py
+++ server/app.py
@@ -65,8 +65,14 @@ def home():
 # Run Server
 # =========================
 
+import os
+
 if __name__ == "__main__":
 
+    port = int(os.environ.get("PORT", 10000))
+
     app.run(
-        debug=True
+        host="0.0.0.0",
+        port=port,
+        debug=False
     )
